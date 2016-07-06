ec706145667ebdbb6e63eae02bd922e968d73160
tiagomartinho <tiago.martinho@xpeppers.com>
2016-07-06 16:54:48 +0200

    initial commit

---
+++ UnitTestProject1/KarateChopTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestEmptyArrayDoesNotFindNumber()
+        {
+            Assert.AreEqual(-1, new KarateChop().Chop(3, new int[0] { }));
+        }
+
+        [TestMethod]
+        public void TestArrayWithoutNumberDoesNotFindNumber()
+        {
+            Assert.AreEqual(-1, new KarateChop().Chop(3, new int[1] {1}));
+        }
+
+        [TestMethod]
+        public void TestArrayWithNumberFindsNumber()
+        {
+            Assert.AreEqual(0, new KarateChop().Chop(1, new int[1] { 1 }));
+        }
+    }
+}
---
+++ WpfApplication1/KarateChop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class KarateChop
+    {
+        public int Chop(int number, int[] numbers) {
+            return -1;
+        }
+    }
+}
